3676b3d6475ae40f87e81b374b68e9646339bd6e
Mukul Pathania <mukulpathania96@gmail.com>
2020-10-13 11:09:45 +0530

    Add some setup to store Database credentials in .env file
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

---
+++ app/io/github/cdimascio/dotenv/DotenvEntry.java
@@ -0,0 +1,37 @@
+package app.io.github.cdimascio.dotenv;
+
+/**
+ * A key value pair representing an environment variable and its value
+ */
+public class DotenvEntry {
+
+    private final String key;
+    private final String value;
+
+    public DotenvEntry(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    /**
+     * Returns the key for the {@link DotenvEntry}
+     * @return the key for the {@link DotenvEntry}
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Returns the value for the {@link DotenvEntry}
+     * @return the value for the {@link DotenvEntry}
+     */
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return key+"="+value;
+    }
+}
+
---
+++ app/io/github/cdimascio/dotenv/DotenvException.java
@@ -0,0 +1,14 @@
+package app.io.github.cdimascio.dotenv;
+
+/**
+ * Signals that dotenv exception of some sort has occurred.
+ */
+public class DotenvException extends RuntimeException {
+    public DotenvException(String message) {
+        super(message);
+    }
+
+    public DotenvException(Throwable cause) {
+        super(cause);
+    }
+}
